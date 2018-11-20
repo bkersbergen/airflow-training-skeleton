29323464942f4d8d8b8d01293f4345ff2787185e
Barrie Kersbergen <bkersbergen@bol.com>
2018-11-20 14:15:13 +0100

    attempt

--- dags/airflow_training/ex3.py
+++ dags/airflow_training/ex3.py
@@ -1,43 +1,64 @@
 import airflow
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.python_operator import BranchPythonOperator
+from airflow.utils.trigger_rule import TriggerRule
 
-args = {"owner": "bkersbergen", "start_date": airflow.utils.dates.days_ago(14)}
+
+weekday_person_to_email = {
+    0: "Bob",
+    1: "Joe",
+    2: "Alice",
+    3: "Joe",
+    4: "Alice",
+    5: "Alice",
+    6: "Alice"
+}
+
+
+args = {"owner": "bkersbergen",
+        "start_date": airflow.utils.dates.days_ago(14)}
+
 dag = DAG(
     dag_id="ex3",
     default_args=args
 )
 
-weekday_person_to_email = {
-    0: "bob",
-    1: "joe",
-    2: "alice",
-    3: "joe",
-    4: "alice",
-    5: "alice",
-    6: "alice",
-}
+def __print_weekday(execution_date, **context):
+    print(execution_date.strftime('%d'))
 
+print_weekday = PythonOperator(
+    task_id="print_exec_date",
+    python_callable=__print_weekday,
+    provide_context=True,
+    dag=dag
+)
 
 def __determine_who_to_email(execution_date, **context):
-    weekday_id = execution_date.strftime('%a')
-    name = weekday_person_to_email[weekday_id]
-    if not name:
-        from airflow import AirflowException
-        raise AirflowException('{} not in weekday_person_to_email'.format(weekday_id))
-    return name
+    name = weekday_person_to_email[execution_date.strftime('%d')]
+    return "email_{}".format(name)
 
 
 branching = BranchPythonOperator(
-    task_id='branching', python_callable=__determine_who_to_email, provide_context=True, dag=dag
+    task_id="branching",
+    python_callable=__determine_who_to_email,
+    provide_context=True,
+    dag=dag
 )
 
-for person in set(weekday_person_to_email.values()):
-    branching >> DummyOperator(task_id=person, dag=dag)
+send_emails = [ PythonOperator(
+    task_id="email_{name}",
+    python_callable=__determine_who_to_email,
+    provide_context=True,
+    dag=dag
+) for name in set(weekday_person_to_email.values())
+]
 
-t1 = BashOperator(
-    task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
+final_task = DummyOperator(
+    task_id="final_task",
+    dag=dag,
+    trigger_rule=TriggerRule.ONE_SUCCESS
 )
+
+print_weekday >> branching >> send_emails >> final_task
